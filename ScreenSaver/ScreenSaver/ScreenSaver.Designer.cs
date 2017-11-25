1062a24f9a04c2679c279133e096a6e3fc1bb441
霧卦 <1103108147@gm.kuas.edu.tw>
2017-11-26 00:27:38 +0800

    有圓圈了
    左上角有圓圈
    25FPS

--- ScreenSaver/ScreenSaver/ScreenSaver.Designer.cs
+++ ScreenSaver/ScreenSaver/ScreenSaver.Designer.cs
@@ -29,10 +29,28 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "ScreenSaver";
             this.Text = "ScreenSaver";
+            this.Load += new System.EventHandler(this.ScreenSaver_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.ScreenSaver_Paint);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- ScreenSaver/ScreenSaver/ScreenSaver.cs
+++ ScreenSaver/ScreenSaver/ScreenSaver.cs
@@ -11,9 +11,54 @@ namespace ScreenSaver
 {
     public partial class ScreenSaver : Form
     {
+        #region 宣告參數
+        List<Circle> listCirCle = new List<Circle>();
+
+        #endregion
         public ScreenSaver()
         {
             InitializeComponent();
+        }
+
+        private void ScreenSaver_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 40;
+            this.DoubleBuffered = true;
+            Circle tmp = new Circle();
+            listCirCle.Add(tmp);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ScreenSaver_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Circle tmp in listCirCle)
+            {
+                tmp.Draw(e.Graphics);
+            }
+        }
+
+        
+    }
+
+    class Circle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        Pen blackPen;
+        Rectangle rect;
+        public Circle()
+        {
+            blackPen = new Pen(Color.Black);
+            rect = new Rectangle(0, 0, 100, 100);
+        }
+
+        public void Draw(Graphics e)
+        {
+            e.DrawArc(blackPen, rect, 0f, 360f);
         }
     }
 }
